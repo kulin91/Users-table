b0012afd93816afe8b450fa4dc1235c4a8c3503d
Alexandr Kulinkovich <alexandr_kulinkovich@icloud.com>
2021-09-13 12:47:15 +0300

    Create component Table

---
+++ src/components/table/Table.js
@@ -0,0 +1,18 @@
+import './style.css';
+
+export default function Table() {
+  return (
+    <div>
+      <table border="1">
+        <tr>
+          <th>ID</th>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>State</th>
+        </tr>
+      </table>
+    </div>
+  );
+}
