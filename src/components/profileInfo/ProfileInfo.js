741cb493e020b853f693d617c56b7ae45ff6320f
Alexandr Kulinkovich <alexandr_kulinkovich@icloud.com>
2021-09-14 11:36:56 +0300

    Created ProfileInfo component

---
+++ src/components/profileInfo/ProfileInfo.js
@@ -0,0 +1,21 @@
+import './style.css';
+
+export default function ProfileInfo(props) {
+  const { selectedProfile } = props;
+  console.log(selectedProfile);
+  return (
+    <div className="profileInfoContainer">
+      {/* <div className="profileInfoBox"> */}
+      <div className="profileInfoTitle">ProfileInfo:</div>
+      <div className="profileInfoSelectedProfile">
+        Selected Profile: {selectedProfile.firstName} {selectedProfile.lastName}
+      </div>
+      <div className="profileInfoDescription">Description: {selectedProfile.description}</div>
+      <div className="profileInfoAdress">Adress: {selectedProfile.adress.streetAddress}</div>
+      <div className="profileInfoSity">City: {selectedProfile.adress.city}</div>
+      <div className="profileInfoState">State: {selectedProfile.adress.state}</div>
+      <div className="profileInfoIndex">Index: {selectedProfile.adress.zip}</div>
+      {/* </div> */}
+    </div>
+  );
+}
